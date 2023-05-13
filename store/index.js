47675a06fa947bfd0181520fa3dd20f7621c0da0
rivaalms <rvalms@icloud.com>
2023-05-13 09:27:10 +0700

    update: vuex for components

--- store/index.js
+++ store/index.js
@@ -1,6 +1,63 @@
 export const state = () => ({
    isAuth: false,
-   api_token: null
+   api_token: null,
+   
+   tableHeader: [
+      {
+         text: 'ID',
+         value: 'id'
+      },
+      {
+         text: 'Nama Sekolah',
+         value: 'school.name'
+      },
+      {
+         text: 'Tipe data',
+         value: 'data_type.name',
+      },
+      {
+         text: 'Kategori',
+         sortable: false,
+         value: 'data_type.data_category.name',
+      },
+      {
+         text: 'Tahun ajaran',
+         value: 'year',
+      },
+      {
+         text: 'Terakhir diperbarui',
+         sortable: false,
+         value: 'date'
+      },
+      {
+         text: 'Aksi',
+         sortable: false,
+         value: 'actions',
+      },
+   ],
+
+   alert: {
+      type: null,
+      icon: null,
+      color: null,
+      message: null
+   },
+   alertTrigger: false,
+
+   snackbar: {
+      color: null,
+      text: null
+   },
+   snackbarTrigger: false,
+
+   breadcrumb: {
+      text: null,
+      disabled: false,
+      href: null
+   },
+
+   dialog: null,
+   dialogTrigger: false
 })
 
 export const mutations = {
@@ -9,6 +66,53 @@ export const mutations = {
    },
    SET_API_TOKEN(state, payload) {
       state.api_token = payload
+   },
+
+   SET_ALERT(state, payload) {
+      state.alert = payload
+   },
+   SHOW_ALERT(state) {
+      state.alertTrigger = true
+   },
+   CLEAR_ALERT(state) {
+      state.alert = {
+         type: null,
+         color: null,
+         icon: null,
+         message: null
+      }
+      state.alertTrigger = false
+   },
+
+   SET_SNACKBAR(state, payload) {
+      state.snackbar = payload
+   },
+   SHOW_SNACKBAR(state) {
+      state.snackbarTrigger = true
+   },
+   CLEAR_SNACKBAR(state) {
+      state.snackbar = {
+         text: null,
+         color: null
+      }
+      state.snackbarTrigger = false
+   },
+
+   SET_BREADCRUMB(state, payload) {
+      state.breadcrumb = payload
+   },
+
+   SET_DIALOG(state, payload) {
+      state.dialog = payload
+   },
+   SHOW_DIALOG(state) {
+      state.dialogTrigger = true
+   },
+   CLEAR_DIALOG(state) {
+      state.dialog = null
+   },
+   CLOSE_DIALOG(state) {
+      state.dialogTrigger = false
    }
 }
 
@@ -18,5 +122,49 @@ export const actions = {
       if(context.app.$auth.$state.loggedIn) {
          commit('SET_API_TOKEN', context.app.$auth.$state.user.api_token)
       }
+   },
+
+   setAlert({commit}, payload) {
+      commit('SET_ALERT', payload)
+   },
+
+   showAlert({commit}) {
+      commit('SHOW_ALERT')
+   },
+
+   clearAlert({commit}) {
+      commit('CLEAR_ALERT')
+   },
+
+   setSnackbar({commit}, payload) {
+      commit('SET_SNACKBAR', payload)
+   },
+
+   showSnackbar({commit}) {
+      commit('SHOW_SNACKBAR')
+   },
+
+   clearSnackbar({commit}) {
+      commit('CLEAR_SNACKBAR')
+   },
+
+   setBreadcrumb({commit}, payload) {
+      commit('SET_BREADCRUMB', payload)
+   },
+
+   setDialog({commit}, payload) {
+      commit('SET_DIALOG', payload)
+   },
+
+   showDialog({commit}) {
+      commit('SHOW_DIALOG')
+   },
+
+   clearDialog({commit}) {
+      commit('CLEAR_DIALOG')
+   },
+
+   closeDialog({commit}) {
+      commit('CLOSE_DIALOG')
    }
 }
